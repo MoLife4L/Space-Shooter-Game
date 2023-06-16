c18483781e2560ec63115482022ebb8d85799a92
Mohamed Daffaallah <103550224+MoLife4L@users.noreply.github.com>
2023-06-16 06:26:22 -0500

    Add files via upload

---
+++ space_shooter_func.py
@@ -0,0 +1,100 @@
+import pygame
+import os 
+pygame.font.init()
+pygame.mixer.init()
+
+WIDTH, HEIGHT = 1500, 900 
+WIN = pygame.display.set_mode((WIDTH, HEIGHT)) #making the window for the game 
+pygame.display.set_caption("Space Game")
+
+white = (255, 255, 255)
+black = (0, 0, 0)
+red_color = (255, 0, 0)
+blue_color = (0, 0, 255)
+
+border = pygame.Rect(WIDTH//2 - 5, 0, 10, HEIGHT)
+
+health_font = pygame.font.SysFont("comicsans", 40)
+winner_font = pygame.font.SysFont("comicsans", 150)
+
+
+vel = 4
+bullet_vel = 6
+spaceship_width, spaceship_height = 90,70
+
+blue_hit = pygame.USEREVENT + 1
+red_hit = pygame.USEREVENT + 2
+
+blue_spaceship_image = pygame.image.load(r'C:\Users\moham\OneDrive\Desktop\Space_Game_Folder\Assets\blue.png')  #loads the image from the appropriate path 
+blue_spaceship = pygame.transform.rotate(pygame.transform.scale(blue_spaceship_image, (spaceship_width, spaceship_height)), 270) # rotates and scales the image 
+
+red_spaceship_image = pygame.image.load(r'C:\Users\moham\OneDrive\Desktop\Space_Game_Folder\Assets\red.png')
+red_spaceship = pygame.transform.rotate(pygame.transform.scale(red_spaceship_image, (spaceship_width, spaceship_height)), 90)
+
+space = pygame.transform.scale(pygame.image.load(r'C:\Users\moham\OneDrive\Desktop\Space_Game_Folder\Assets\spacemove.png'), (WIDTH, HEIGHT))
+
+def draw_window(blue, red, blue_bullets, red_bullets, blue_health, red_health):
+    WIN.blit(space, (0,0))
+    pygame.draw.rect(WIN, white, border) #draws an item in the window that is black and is the border
+
+    blue_health_text = health_font.render("Blue Health: " + str(blue_health), 1, white)
+    red_health_text = health_font.render("Red Health: " + str(red_health), 1, white)
+    WIN.blit(blue_health_text, (250, 10))
+    WIN.blit(red_health_text, (1000,10))
+
+    WIN.blit(blue_spaceship, (blue.x, blue.y)) #blit puts items onto the screen. This is blitting the spaceship on screen
+    WIN.blit(red_spaceship, (red.x,red.y))
+
+
+
+    for bullet in blue_bullets:
+        pygame.draw.rect(WIN, blue_color, bullet)
+
+    for bullet in red_bullets:
+        pygame.draw.rect(WIN, red_color, bullet)
+
+    pygame.display.update() #updates the display 
+
+def blue_movement(keys_pressed, color):
+    if keys_pressed[pygame.K_a] and color.x - vel > 0: #blue ship moving Left
+        color.x -= vel
+    if keys_pressed[pygame.K_d] and color.x + vel + color.width < border.x: #blue ship moving Right
+        color.x += vel
+    if keys_pressed[pygame.K_w] and color.y - vel > 0: #blue ship moving up
+        color.y -= vel
+    if keys_pressed[pygame.K_s] and color.y + vel + color.height < border.height - 15: #blue ship moving down
+        color.y += vel
+
+def red_movement(keys_pressed, color):
+    if keys_pressed[pygame.K_LEFT] and color.x - vel > border.x + border.width: #red ship moving Left
+        color.x -= vel
+    if keys_pressed[pygame.K_RIGHT] and color.x + vel + color.width < WIDTH + 20: #red ship moving Right
+        color.x += vel
+    if keys_pressed[pygame.K_UP] and color.y - vel > 0: #red ship moving up
+        color.y -= vel
+    if keys_pressed[pygame.K_DOWN] and color.y + vel + color.height < border.height - 15: #red ship moving down
+        color.y += vel
+
+def handle_bullets(blue_bullets, red_bullets, blue, red):
+    for bullet in blue_bullets:
+        bullet.x += bullet_vel
+        if red.colliderect(bullet):
+            pygame.event.post(pygame.event.Event(red_hit))
+            blue_bullets.remove(bullet)
+        elif bullet.x > WIDTH:
+            blue_bullets.remove(bullet)
+
+
+    for bullet in red_bullets:
+        bullet.x -= bullet_vel
+        if blue.colliderect(bullet):
+            pygame.event.post(pygame.event.Event(blue_hit))
+            red_bullets.remove(bullet)
+        elif bullet.x < 0:
+            red_bullets.remove(bullet) 
+
+def draw_winner(text):
+    draw_text = winner_font.render(text, 1, white)
+    WIN.blit(draw_text, (400, 100))
+    pygame.display.update()
+    pygame.time.delay(5000)
---
+++ space_shooter_main.py
@@ -0,0 +1,73 @@
+import pygame
+import os 
+pygame.font.init()
+pygame.mixer.init()
+
+from space_shooter_func import draw_window, blue_movement, red_movement, handle_bullets, draw_winner, blue_hit, red_hit, spaceship_height, spaceship_width
+
+max_bullets = 10
+
+fps = 60
+
+bullet_sound = pygame.mixer.Sound(r'C:\Users\moham\OneDrive\Desktop\Space_Game_Folder\Assets\shot.mp3')
+explosion_sound = pygame.mixer.Sound(r'C:\Users\moham\OneDrive\Desktop\Space_Game_Folder\Assets\explosion.mp3')
+
+def main():
+    blue = pygame.Rect(100, 375, spaceship_width, spaceship_height)
+    red = pygame.Rect(1300, 375, spaceship_width, spaceship_height)
+
+    blue_bullets = []
+    red_bullets = []
+    blue_health = 10
+    red_health = 10
+
+    clock = pygame.time.Clock() 
+    run = True 
+    while run:
+        clock.tick(fps) #locks the fps so it isn't open ended
+        for event in pygame.event.get():   #loops through all events in the game 
+            if event.type == pygame.QUIT:  #if user exits the window 
+                run = False #ends the while loop which quits the game 
+                pygame.quit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LCTRL and len(blue_bullets) < max_bullets:
+                    bullet = pygame.Rect(blue.x + blue.width, blue.y + blue.height//2 +5, 10, 5)
+                    blue_bullets.append(bullet)
+                    bullet_sound.play()
+
+                if event.key == pygame.K_RCTRL and len(red_bullets) < max_bullets:
+                    bullet = pygame.Rect(red.x, red.y + red.height//2 +5, 10, 5)
+                    red_bullets.append(bullet)
+                    bullet_sound.play()
+
+            if event.type == red_hit:
+                red_health -= 1
+                explosion_sound.play()
+            
+            if event.type == blue_hit:
+                blue_health -= 1
+                explosion_sound.play()
+                
+        winner = ""        
+        if blue_health <=0:
+            winner = "Red Wins!"
+        
+        if red_health <= 0:
+            winner = "Blue Wins!"
+        
+        if winner != "":
+            draw_winner(winner)
+            break
+       
+        keys_pressed = pygame.key.get_pressed()
+        blue_movement(keys_pressed, blue)
+        red_movement(keys_pressed, red)
+        handle_bullets(blue_bullets, red_bullets, blue, red)
+        draw_window(blue, red, blue_bullets, red_bullets, blue_health, red_health)
+
+    main()
+
+
+if __name__ == "__main__":
+    main()
